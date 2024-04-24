a9b747eaa07a571a975951f3345ab20a1a832622
Carlos Sánchez Donate <43948976+barvock@users.noreply.github.com>
2024-04-24 21:58:09 +0200

    funciona

--- descargador-asdrubalis/descargador-asdrubalis.php
+++ descargador-asdrubalis/descargador-asdrubalis.php
@@ -27,12 +27,74 @@ function image_downloader_menu() {
 function image_downloader_admin_page() {
     ?>
     <div class="wrap">
-        <h1><?php echo esc_html__('Descargador de Archivos', 'descargador-asdrubalis'); ?></h1>
-        <form method="post" action="">
-            <?php wp_nonce_field('download_images', 'image_downloader_nonce'); ?>
-            <textarea name="image_urls" style="width: 100%; height: 100px;"></textarea><br>
-            <input type="submit" value="<?php echo esc_attr__('Descargar Archivos', 'descargador-asdrubalis'); ?>" class="button button-primary">
-        </form>
+        <h1><?php echo esc_html__('Descargador de Archivos', 'descargador-asdrubalis'); ?> Asdrubal SEO SL</h1>
+		<form id="asdruform" method="post" action="">
+			<?php wp_nonce_field('download_images', 'image_downloader_nonce'); ?>
+			<p>Incluye aqui la URL de todos los elementos que deseas descargar:</p>
+			<ol>
+				<li>Una URL por linea</li>
+				<li>Si no seleccionas ninguna opción posterior se respetará la estructura de la URL a la hora de la descarga</li>
+				<li>A menos que se especifique, las imágenes no serán sobreescritas en caso de coincidir</li>
+			</ol>
+			<textarea name="image_urls" style="width: 100%; height: 100px;"></textarea><br>
+			<div id="folder-selector">
+				<input type="text" name="custom_folder" id="folder-path" placeholder="<?php echo esc_attr__('Ruta de la Carpeta (Opcional)', 'descargador-asdrubalis'); ?>" style="width: 80%;">
+				<button type="button" id="select-folder-btn" class="button"><?php echo esc_html__('Seleccionar Carpeta', 'descargador-asdrubalis'); ?></button>
+			</div>
+			<br>
+
+			<label>
+				<input type="checkbox" name="flatten_folders" value="1"> 
+				<?php echo esc_html__('Guardar todos los archivos directamente en la carpeta especificada, sin mantener la estructura de directorios.', 'descargador-asdrubalis'); ?>
+			</label><br>
+            <label>
+                <input type="checkbox" name="appear_in_media_library" value="1">
+                <?php echo esc_html__('Aparecer en la biblioteca de medios', 'descargador-asdrubalis'); ?>
+            </label><br>
+			<label>
+				<input type="checkbox" name="overwrite_files" value="1">
+				<b>Sobrescribir</b> archivos existentes. <b>¡Cuidado! Esta acción no se puede deshacer.</b>
+			</label><br>
+			<details>
+				<summary class="desplegable-advanced" title="Haz click para opciones avanzadas">Avanzado</summary>
+				<div class="advanced-box">
+					<div>
+						<label for="user_agent">User-Agent (opcional):</label>
+						<input type="text" name="user_agent" id="user_agent" placeholder="Mozilla/5.0 (Windows NT 10.0; Win64; x64)" style="width: 100%;">
+					</div>
+					<div>
+						<label for="download_delay">Retardo entre descargas (en segundos, opcional):</label>
+						<input type="number" name="download_delay" placeholder="0" id="download_delay" min="0" style="width: 100%;">
+					</div>
+					<div>
+						<label for="max_file_size">Tamaño máximo de un archivo:</label>
+						<div class="max_size_asdr">
+							<input type="number" name="max_file_size" id="max_file_size" min="0" style="width: 80%;">
+							<select name="max_file_size_unit" id="max_file_size_unit" style="width: 20%;">
+								<option value="1024">KB</option>
+								<option value="1048576" selected>MB</option>
+								<option value="1073741824">GB</option>
+							</select>
+						</div>
+					</div>
+					<div>
+						<label for="max_total_size">Tamaño máximo total de descarga:</label>
+						<div class="max_size_asdr">
+							<input type="number" name="max_total_size" id="max_total_size" min="0" style="width: 80%;">
+							<select name="max_total_size_unit" id="max_total_size_unit" style="width: 20%;">
+								<option value="1024">KB</option>
+								<option value="1048576" selected>MB</option>
+								<option value="1073741824">GB</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</details>
+			<input type="submit" value="<?php echo esc_attr__('Descargar Archivos', 'descargador-asdrubalis'); ?>" class="button button-primary">	
+		</form>
+
+
+
     </div>
     <?php
     if (!empty($_POST['image_urls']) && check_admin_referer('download_images', 'image_downloader_nonce')) {
@@ -40,29 +102,73 @@ function image_downloader_admin_page() {
     }
 }
 
-// Función para manejar la descarga de Archivos
 function handle_image_download() {
     $downloaded_files = [];
     $urls = explode("\n", sanitize_textarea_field($_POST['image_urls']));
+    $appearInMediaLibrary = isset($_POST['appear_in_media_library']) && $_POST['appear_in_media_library'] == '1';
+    $custom_folder = sanitize_textarea_field($_POST['custom_folder']);
+    $flatten_folders = isset($_POST['flatten_folders']) && $_POST['flatten_folders'] == '1';
+    $overwrite_files = isset($_POST['overwrite_files']) && $_POST['overwrite_files'] == '1';
+    $userAgent = sanitize_text_field($_POST['user_agent']) ?: 'Mozilla/5.0 (WordPress/DescargadorAsdrubalis; +https://carlos.sanchezdonate.com)';
+    $downloadDelay = intval($_POST['download_delay']);
+    $maxFileSize = intval($_POST['max_file_size']) * intval($_POST['max_file_size_unit']);
+    $maxTotalSize = intval($_POST['max_total_size']) * intval($_POST['max_total_size_unit']);
+    $totalSize = 0;
+
     foreach ($urls as $url) {
         $url = trim($url);
         if (!empty($url) && filter_var($url, FILTER_VALIDATE_URL)) {
             $parsedUrl = parse_url($url);
             $path = $parsedUrl['path'];
+
+            if ($flatten_folders && !empty($custom_folder)) {
+                $filename = basename($path);
+                $path = '/' . trim($custom_folder, '/') . '/' . $filename;
+            } elseif (!empty($custom_folder)) {
+                $path = '/' . trim($custom_folder, '/') . $path;
+            }
+
             $localPath = ABSPATH . $path;
 
-            if (!file_exists(dirname($localPath))) {
+            // Crear directorio si no existe
+             if (!file_exists(dirname($localPath))) {
                 wp_mkdir_p(dirname($localPath));
             }
-            file_put_contents($localPath, file_get_contents($url));
-            $downloaded_files[] = $localPath; // Registrar el archivo descargado
+
+            $fileContents = file_get_contents($url, false, $context);
+
+            if ($fileContents !== false && (!$maxFileSize || strlen($fileContents) <= $maxFileSize)) {
+                if ($overwrite_files || !file_exists($localPath)) {
+                    if (!$maxTotalSize || ($totalSize + strlen($fileContents) <= $maxTotalSize)) {
+                        file_put_contents($localPath, $fileContents);
+                        if ($appearInMediaLibrary && wp_upload_dir()['basedir'] === dirname($localPath)) {
+                            // Registrar en la biblioteca de medios si está en uploads y seleccionado
+                            $file_array = ['tmp_name' => $localPath, 'name' => basename($localPath)];
+                            $attachment_id = media_handle_sideload($file_array, 0);
+                            if (!is_wp_error($attachment_id)) {
+                                $downloaded_files[] = wp_get_attachment_url($attachment_id);
+                            }
+                        } else {
+                            $downloaded_files[] = $localPath;
+                        }
+                        $totalSize += strlen($fileContents); // Sumar al total
+                        if ($downloadDelay) sleep($downloadDelay); // Pausar entre descargas
+                    } else {
+                        echo '<div class="notice notice-warning"><p>' . esc_html__('No se completó la descarga de todos los archivos debido al límite total de tamaño de descarga.', 'descargador-asdrubalis') . '</p></div>';
+                        break;
+                    }
+                }
+            } else {
+                echo '<div class="notice notice-error"><p>' . sprintf(esc_html__('Archivo "%s" no descargado: excede el tamaño máximo permitido por archivo o es inaccesible.', 'descargador-asdrubalis'), $url) . '</p></div>';
+            }
         }
     }
 
     // Guardar el registro de descargas en la opción de WordPress
     update_option('recent_downloads', json_encode($downloaded_files));
-    echo '<div class="updated"><p>' . esc_html__('Descarga completada.', 'descargador-asdrubalis') . '</p></div>';
+    echo '<div class="updated"><p>' . esc_html__('Descarga completada con éxito.', 'descargador-asdrubalis') . '</p></div>';
 }
+
 
 add_action('admin_menu', 'add_undo_page');
 
@@ -96,5 +202,49 @@ function undo_downloads_page() {
         }
     }
 }
+
+add_action('wp_ajax_get_directory_contents', 'get_directory_contents');
+
+function get_directory_contents() {
+    check_ajax_referer('image_downloader_nonce', 'security');
+
+    $path = sanitize_text_field($_POST['path']);
+    $fullPath = ABSPATH . untrailingslashit($path);
+
+    if (!current_user_can('manage_options') || !is_dir($fullPath)) {
+        wp_send_json_error(array('message' => 'No se pudo acceder al directorio.'));
+    }
+
+    $directories = array();
+    $dir = new DirectoryIterator($fullPath);
+    foreach ($dir as $fileinfo) {
+        if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+            $directories[] = $fileinfo->getFilename();
+        }
+    }
+
+    wp_send_json_success($directories);
+}
+
+
+// Mp encriptar
+function image_downloader_scripts() {
+    wp_enqueue_script('folder-selector-js', plugin_dir_url(__FILE__) . 'js/folder-selector.js', array(), '1.0', true);
+    wp_localize_script('folder-selector-js', 'imageDownloader', array(
+        'ajaxurl' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('image_downloader_nonce')
+    ));
+}
+add_action('admin_enqueue_scripts', 'image_downloader_scripts');
+
+function image_downloader_styles() {
+    // Asegúrate de que el path a tu archivo CSS es correcto
+    wp_enqueue_style('image-downloader-css', plugin_dir_url(__FILE__) . 'css/style.css', array(), '1.0', 'all');
+}
+
+add_action('admin_enqueue_scripts', 'image_downloader_styles');
+
+
+
 
 ?>
